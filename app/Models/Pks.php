37a289102df82f02f3c941ca1160f0812aa3996f
Priyo <priyobanget@gmail.com>
2022-02-03 16:19:13 +0700

    update sidebar

--- app/Models/Pks.php
+++ app/Models/Pks.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Pks extends Model
 {
@@ -12,4 +13,9 @@ class Pks extends Model
     protected $table='M_PKS';
     protected $primaryKey = 'KODE';
     protected $keyType = 'string';
+
+    public function getNama2Attribute()
+    {
+        return Str::of(Str::title($this->NAMA))->replace('Pks','PKS')->replace('Pabrik Kelapa Sawit', 'PKS');
+    }
 }
