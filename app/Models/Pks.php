9c32e50da2bf5f62d185e636aff81c34f3905ffc
Priyo <priyobanget@gmail.com>
2022-02-04 08:57:14 +0700

    update API PKS

--- app/Models/Pks.php
+++ app/Models/Pks.php
@@ -15,8 +15,24 @@ class Pks extends Model
     protected $keyType = 'string';
     protected $appends = ['nama2'];
 
+    public function company()
+    {
+        return $this->belongsTo(Company::class, 'COMPANY_CODE', 'KODE');
+    }
+
     public function getNama2Attribute()
     {
         return Str::of(Str::title($this->NAMA))->replace('Pks','PKS')->replace('Pabrik Kelapa Sawit', 'PKS');
     }
+
+    public function getNamaCompanyAttribute()
+    {
+        return $this->company->NAMA;
+    }
+
+    public function getNamaCompanyPanjangAttribute()
+    {
+        return Str::of($this->company->NAMA)->replace('PTPN','PT Perkebunan Nusantara');
+    }
+
 }
